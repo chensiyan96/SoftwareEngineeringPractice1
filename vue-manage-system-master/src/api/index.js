6eddbb26527c374e392ce13c235851967182d270
yzhHoward <yzhhoward@gmail.com>
2019-08-23 14:48:48 +0800

    换模板啦！

--- vue-manage-system-master/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import request from '../utils/request';
-
-export const fetchData = (query) => {
-    return request({
-        url: '/ms/table/list',
-        method: 'post',
-        data: query
-    })
-}
