374150547a001267cb4f584efa75d0db07eff353
yzhHoward <yzhhoward@gmail.com>
2019-09-01 21:55:56 +0800

    update

--- dbblog-master/dbblog-frontend/static/config/index-dev.js
+++
@@ -1,9 +0,0 @@
-/**
- * 开发环境
- */
-;(function () {
-  window.SITE_CONFIG = {}
-  // api接口请求地址
-  // window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/dbblog/'
-  window.SITE_CONFIG['baseUrl'] = 'http://47.94.106.116:8080/dbblog'
-})()
--- dbblog-master/dbblog-frontend/static/config/index-prod.js
+++
@@ -1,8 +0,0 @@
-/**
- * 生产环境
- */
-;(function () {
-  window.SITE_CONFIG = {}
-  // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://47.94.106.116:8080/dbblog'
-})()
